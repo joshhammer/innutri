4c26f771669157f214002367ab61e66acd3d52ae
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-04-23 11:35:16 +0200

    hide scrollbar and change display of app bg

--- src/App.js
+++ src/App.js
@@ -11,16 +11,14 @@ import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-      <div className='app-backdrop'>
-        <div className='wrapper'>
-          <Header />
-          <Intro />
-          <Advertiser />
-          <Usp />
-          <Science />
-          <Products />
-          <Footer />
-        </div>
+      <div className='wrapper'>
+        <Header />
+        <Intro />
+        <Advertiser />
+        <Usp />
+        <Science />
+        <Products />
+        <Footer />
       </div>
     </div>
   );
